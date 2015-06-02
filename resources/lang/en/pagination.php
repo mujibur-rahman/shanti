a0ea3ecbbed79c2f33675df107f8e859945e3ade
Mujibur Rahman <mujibur.rahman@cention.se>
2015-06-02 21:29:33 +0800

    Add pagination translation function. Custom pagination added over there

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -13,7 +13,16 @@ return [
 	|
 	*/
 
-	'previous' => '&laquo; Previous',
-	'next'     => 'Next &raquo;',
+		'previous' => '&laquo; Previous',
+		'next'     => 'Next &raquo;',
+		'1'		=> '1',
+		'2'		=> '2',
+		'3'		=> '3',
+		'4'		=> '4',
+		'5'		=> '5',
+		'6'		=> '6',
+		'7'		=> '7',
+		'8'		=> '8',
+		'9'		=> '9',
 
 ];
