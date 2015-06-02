d61732c2663c37b5ea0e96e6dbccbbcad9b54f4a
Mujibur Rhaman <mujib.rahman.bd@gmail.com>
2015-06-02 08:23:28 -0700

    Menu width fix by padding-right

--- resources/views/front/menu.blade.php
+++ resources/views/front/menu.blade.php
@@ -5,17 +5,17 @@
 					<div class="navbar-inner">
 						<!-- Menu -->
 						<ul class="nav navbar-nav l_tinynav1" id="nav">
-						<li><a href="/" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.home')}}</a></li>
-						<li><a href="/category/article/1" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!}>
+						<li><a href="/" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.home')}}</a></li>
+						<li><a href="/category/article/1" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!}>
 						{{ trans('front/menu.restaurant')}} 
 						</a></li>      
-						<li><a href="/category/article/3" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.club')}} </a></li>
-						<li><a href="/category/article/4" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.science')}} </a></li> 
-						<li><a href="/category/article/5" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.shopping')}} </a></li>    
-						<li><a href="/category/article/6" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.citylife')}} </a></li>
-						<li><a href="http://www.shantidhaka.com/blog/" target="_blank" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.blog')}} </a></li> 
-						<li><a href="/category/article/7" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.guide')}} </a></li> 
-						<li><a href="/category/article/8" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 17px !important; border: 1px solid #5b575b !important;padding-right: 37px;"' : '')  !!} > &nbsp;&nbsp;MORE <span class="caret"></span></a>
+						<li><a href="/category/article/3" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.club')}} </a></li>
+						<li><a href="/category/article/4" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.science')}} </a></li> 
+						<li><a href="/category/article/5" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.shopping')}} </a></li>    
+						<li><a href="/category/article/6" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.citylife')}} </a></li>
+						<li><a href="http://www.shantidhaka.com/blog/" target="_blank" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!}>{{ trans('front/menu.blog')}} </a></li> 
+						<li><a href="/category/article/7" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 20px !important; border: 1px solid #5b575b !important;"' : '')  !!} >{{ trans('front/menu.guide')}} </a></li> 
+						<li><a href="/category/article/8" title="" {!! (session('locale') == 'bg' ? 'style="font-size: 25px !important; border: 1px solid #5b575b !important;padding-right: 37px;"' : 'style="padding-right: 27px;"')  !!} > &nbsp;&nbsp;MORE <span class="caret"></span></a>
 							<!-- Submenu -->
 							<ul>
 							<li><a href="/category/article/15" title="">{{ trans('front/menu.child')}} </a></li>
